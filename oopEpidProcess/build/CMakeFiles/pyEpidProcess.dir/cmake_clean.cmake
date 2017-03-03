1a9b6db94d6339388e4bc1fa06292e20e085ff78
Zhanibek <zhanibek.om@gmail.com>
2017-03-03 19:57:43 +0900

    change cmake

---
+++ oopEpidProcess/build/CMakeFiles/pyEpidProcess.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pyEpidProcess.dir/epidemicProcess.cpp.o"
+  "CMakeFiles/pyEpidProcess.dir/Linked_list.cpp.o"
+  "libpyEpidProcess.pdb"
+  "libpyEpidProcess.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pyEpidProcess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
